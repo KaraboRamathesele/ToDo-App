e9e3586e197432053ebe782249939c9f4ff8f75a
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-09-11 17:11:17 +0200

    input.value set to anyinput

--- todoApp/main.js
+++ todoApp/main.js
@@ -13,7 +13,20 @@ function addtodo(event) {
     listDiv.classList.add('lists');
 
     const addList = document.createElement('li');
-    addList.classList.add('addList');
+    addList.innerText = input.value;
+    addList.classList.add('add-list');
+    listDiv.appendChild(addList);
 
+    const buttonAdd = document.createElement('button');
+    buttonAdd.innerText = 'Done';
+    buttonAdd.classList.add('add-btn');
+    listDiv.appendChild(buttonAdd);
+
+    const buttonRemove = document.createElement('button');
+    buttonRemove.innerText = 'remove';
+    buttonRemove.classList.add('remove-btn');
+    listDiv.appendChild(buttonRemove);
+
+    list.appendChild(listDiv);
 
 }
